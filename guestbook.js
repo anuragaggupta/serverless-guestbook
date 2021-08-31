ad5460190dbeceee0ac92d5b92d06530edc85132
Anurag Gupta <anuragaggupta@hotmail.com>
2021-08-31 23:22:32 +0530

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://1118f01a.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
